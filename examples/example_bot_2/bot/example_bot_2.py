d311d78f69c5d2d6b4c0a7ba52a88944ede0ad38
Harry <harryxie.dev@gmail.com>
2021-02-26 06:33:13 +0000

    added more logging to example_bot_2.py

--- examples/example_bot_2/bot/example_bot_2.py
+++ examples/example_bot_2/bot/example_bot_2.py
@@ -11,5 +11,6 @@ class ExampleBot2(ChaiBot):
 
     async def on_message(self, update: Update) -> str:
         messages = await self.get_messages(update.conversation_id)
+        self.logger.info(f"Received {len(messages)} messages.")
         string_count = sum(1 for message in messages if self.STRING in message.content)
         return f"The string has been said {string_count} times."
