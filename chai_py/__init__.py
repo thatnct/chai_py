6b7b981a3f729787d95a13a4c8b88ad187f5f34a
Harry <harryxie.dev@gmail.com>
2021-03-28 15:52:35 +0100

    add imports

--- chai_py/__init__.py
+++ chai_py/__init__.py
@@ -2,4 +2,5 @@ from .chai_bot import ChaiBot
 from .types import LatestMessage, Update, Message, MessageKind
 from .t_room import TRoom
 from .packaging import package, Metadata
-from .deployment import upload_and_deploy, wait_for_deployment, get_bot_status
+from .deployment import upload_and_deploy, wait_for_deployment, get_bot_status, share_bot
+from .notebook_utils import IS_NOTEBOOK, setup_notebook
